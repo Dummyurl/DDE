1060dd1d080a1ae8040109a7738c37c808fb4d11
prathamApp <prathambaner123@gmail.com>
2018-06-21 17:10:32 +0530

    ketan

--- app/src/main/java/pratham/dde/domain/Answer.java
+++ app/src/main/java/pratham/dde/domain/Answer.java
@@ -1,8 +1,10 @@
 package pratham.dde.domain;
 
+import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+@Entity
 public class Answer {
 
     @PrimaryKey
