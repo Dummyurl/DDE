531ad4c226ef99ced59fc3294ed3f0284f828ef9
prathamApp <prathambaner123@gmail.com>
2018-06-21 17:03:52 +0530

    ketan

--- app/src/main/java/pratham/dde/activities/MainActivity.java
+++ app/src/main/java/pratham/dde/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package pratham.dde.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import pratham.dde.BaseActivity;
@@ -12,5 +11,17 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*
+        <?xml version = "1.0" encoding = "utf-8" ?>
+<shape xmlns:android = "http://schemas.android.com/apk/res/android"
+        android:
+        shape = "rectangle" >
+    <corners android:radius = "20dp" / >
+    <solid android:color = "@color/semiTransparentWhite" / >
+</shape >
+*/
+
+
     }
 }
