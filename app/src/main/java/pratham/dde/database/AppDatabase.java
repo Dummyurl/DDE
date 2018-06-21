800ca52bd4dc1a439b4f5c9647c1e76abe0ecf0d
prathamApp <prathambaner123@gmail.com>
2018-06-21 17:03:35 +0530

    pk commits

--- app/src/main/java/pratham/dde/database/AppDatabase.java
+++ app/src/main/java/pratham/dde/database/AppDatabase.java
@@ -3,6 +3,14 @@ package pratham.dde.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import pratham.dde.dao.AnswerDao;
+import pratham.dde.dao.DDE_FormWiseDataSourceDao;
+import pratham.dde.dao.DDE_FormsDao;
+import pratham.dde.dao.DDE_QuestionsDao;
+import pratham.dde.dao.DDE_RuleConditionDao;
+import pratham.dde.dao.DDE_RuleMasterDao;
+import pratham.dde.dao.DDE_RuleQuestionDao;
+import pratham.dde.dao.UserDao;
 import pratham.dde.domain.Answer;
 import pratham.dde.domain.DDE_FormWiseDataSource;
 import pratham.dde.domain.DDE_Forms;
@@ -12,29 +20,34 @@ import pratham.dde.domain.DDE_RuleMaster;
 import pratham.dde.domain.DDE_RuleQuestion;
 import pratham.dde.domain.User;
 
-@Database(entities = { User.class,
-                       DDE_Forms.class,
-                       DDE_Questions.class,
-                       DDE_FormWiseDataSource.class,
-                       DDE_RuleQuestion.class,
-                       DDE_RuleMaster.class,
-                       DDE_RuleCondition.class,
-                       Answer.class}, version = 1)
+@Database(entities = {User.class,
+        DDE_Forms.class,
+        DDE_Questions.class,
+        DDE_FormWiseDataSource.class,
+        DDE_RuleQuestion.class,
+        DDE_RuleMaster.class,
+        DDE_RuleCondition.class,
+        Answer.class}, version = 1)
 
 public abstract class AppDatabase extends RoomDatabase {
 
     public static final String DB_NAME = "dynamic_data_entry";
 
-    public abstract CrlDao getCrlDao();
 
-    public abstract StudentDao getStudentDao();
+    public abstract UserDao getUserDao();
 
-    public abstract ScoreDao getScoreDao();
+    public abstract DDE_FormsDao getDDE_FormsDao();
 
-    public abstract SessionDao getSessionDao();
+    public abstract DDE_QuestionsDao getDDE_QuestionsDao();
 
-    public abstract AttendanceDao getAttendanceDao();
+    public abstract DDE_FormWiseDataSourceDao getDDE_FormWiseDataSourceDao();
 
-    public abstract StatusDao getStatusDao();
+    public abstract DDE_RuleQuestionDao getDDE_RuleQuestionDao();
+
+    public abstract DDE_RuleMasterDao getDDE_RuleMasterDao();
+
+    public abstract DDE_RuleConditionDao getDDE_RuleConditionDao();
+
+    public abstract AnswerDao getAnswerDao();
 
 }
